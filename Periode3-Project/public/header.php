0f97acb3a00898248a24b679aeca7eafe338a888
MagicGnome-UwU <casperveneman25@gmail.com>
2022-03-14 15:17:41 +0100

    php project periode 3

---
+++ Periode3-Project/public/header.php
@@ -0,0 +1,38 @@
+<?php ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/hompage.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <script src="js/jQuery/jquery-3.6.0.min.js"></script>
+    <script src="js/index.js"></script>
+    <title>**INSERT WEBSITE NAME**</title>
+</head>
+<body>
+<section class="top-nav">
+    <div>
+        <a class="logo">LOGO HERE</a>
+    </div>
+    <input id="menu-toggle" type="checkbox" />
+    <label class='menu-button-container' for="menu-toggle">
+        <div class='menu-button'></div>
+    </label>
+    <ul class="menu">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">AdminPage</a></li>
+        <li class="logInIconLI"><a href="#"><img class="logInIcon" src="img/LoggedInIcon.png"><span class="LogInText">Account</span></a></li>
+        <li><a href="#">Log In</a></li>
+        <li><a href="#">Sign Up</a></li>
+
+    </ul>
+</section>
---
+++ Periode3-Project/public/js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(()=>{
+    $(".menu-toggle").on("click", ()=>{
+        $(".menuLI").css('display', 'none')
+    })
+})
