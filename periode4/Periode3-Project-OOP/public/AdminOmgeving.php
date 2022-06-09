3600f7a25c66ec3bf92b145a8af724b6721b51c0
Casper Bruning <82331679+MagicGnome-UwU@users.noreply.github.com>
2022-06-09 11:47:21 +0200

    1
    1

--- periode4/Periode3-Project-OOP/public/AdminOmgeving.php
+++ periode4/Periode3-Project-OOP/public/AdminOmgeving.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('header.php');
+require_once('header.php');
 
 $productID = '';
 $productNaam = '';
@@ -7,7 +7,7 @@ $typeComponent = '';
 $merk = '';
 $prijs = '';
 $update = false;
-if(isset($_GET['edit'])){
+if (isset($_GET['edit'])) {
     $id = $_GET['edit'];
     $update = true;
     $product = new Producten();
@@ -34,34 +34,36 @@ if(isset($_GET['edit'])){
                 <input type="text" id="search">
                 <table>
                     <thead>
-                        <tr>
-                            <th class="productenTableTH">ProductID</th>
-                            <th class="productenTableTH">Product Naam</th>
-                            <th class="productenTableTH">Type Component</th>
-                            <th colspan="2" class="productenTableTH">Action</th>
-                        </tr>
+                    <tr>
+                        <th class="productenTableTH">ProductID</th>
+                        <th class="productenTableTH">Product Naam</th>
+                        <th class="productenTableTH">Type Component</th>
+                        <th colspan="2" class="productenTableTH">Action</th>
+                    </tr>
                     </thead>
             </div>
             <div>
                 <tbody id="filter">
-                <?php $producten = db_getData("SELECT * FROM producten")  ?>
-                    <?php if ($producten -> num_rows > 0){
-                        while ($rows = $producten -> fetch_array()){
-                            ?>
-                            <tr>
-                                <td class="productenTableTD"><?php echo $rows['productID']?></td>
-                                <td class="productenTableTD"><?php echo $rows['naam']?></td>
-                                <td class="productenTableTD"><?php echo $rows['typecomponent']?></td>
-                                <td>
-                                    <a href="AdminOmgeving.php?edit=<?php echo $rows['productID']; ?>" class="edit_btn" >View</a>
-                                </td>
-                                <td>
-                                    <a href="../src/server.php?del=<?php echo $rows['productID']; ?>" class="del_btn">Delete</a>
-                                </td>
-                            </tr>
-                            <?php
-                        }
-                    }?>
+                <?php $producten = db_getData("SELECT * FROM producten") ?>
+                <?php if ($producten->num_rows > 0) {
+                    while ($rows = $producten->fetch_array()) {
+                        ?>
+                        <tr>
+                            <td class="productenTableTD"><?php echo $rows['productID'] ?></td>
+                            <td class="productenTableTD"><?php echo $rows['naam'] ?></td>
+                            <td class="productenTableTD"><?php echo $rows['typecomponent'] ?></td>
+                            <td>
+                                <a href="AdminOmgeving.php?edit=<?php echo $rows['productID']; ?>"
+                                   class="edit_btn">View</a>
+                            </td>
+                            <td>
+                                <a href="../src/server.php?del=<?php echo $rows['productID']; ?>"
+                                   class="del_btn">Delete</a>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                } ?>
                 </tbody>
                 </table>
             </div>
@@ -72,37 +74,39 @@ if(isset($_GET['edit'])){
                     <label>ProductID</label>
                     <input readonly type="text" name="productID" value="<?php echo $productID ?>">
                     <label>ProductNaam</label>
-                    <input type="text" name="naam" value="<?php echo $productNaam?>">
+                    <input type="text" name="naam" value="<?php echo $productNaam ?>">
                     <label>typeComponent</label>
-<!--                    <input type="text" name="typecomponent" value="--><?php //echo $typeComponent?><!--">-->
+                    <!--                    <input type="text" name="typecomponent" value="-->
+                    <?php //echo $typeComponent?><!--">-->
                     <select class="dropdown" name="typecomponent">
-                        <option <?php if ($typeComponent == "case_atx"){echo 'selected';}?> value="case_atx">case_atx</option>
-                        <option <?php if ($typeComponent == "case_microatx"){echo 'selected';}?> value="case_microatx">case_microatx</option>
-                        <option <?php if ($typeComponent == "case_miniitx"){echo 'selected';}?>value="case_miniitx">case_miniitx</option>
-                        <option <?php if ($typeComponent == "cpu_amd"){echo 'selected';}?>value="cpu_amd">cpu_amd</option>
-                        <option <?php if ($typeComponent == "cpu_intel"){echo 'selected';}?>value="cpu_intel">cpu_intel</option>
-                        <option <?php if ($typeComponent == "ram"){echo 'selected';}?>value="ram">ram</option>
-                        <option <?php if ($typeComponent == "cpukoeler"){echo 'selected';}?>value="cpukoeler">cpukoeler</option>
-                        <option <?php if ($typeComponent == "mobo_atx"){echo 'selected';}?>value="mobo_atx">mobo_atx</option>
-                        <option <?php if ($typeComponent == "mobo_microatx"){echo 'selected';}?>value="mobo_microatx">mobo_microatx</option>
-                        <option <?php if ($typeComponent == "mobo_miniitx"){echo 'selected';}?>value="mobo_miniitx">mobo_miniitx</option>
-                        <option <?php if ($typeComponent == "gpu_nvidia"){echo 'selected';}?>value="gpu_nvidia">gpu_nvidia</option>
-                        <option <?php if ($typeComponent == "gpu_radeon"){echo 'selected';}?>value="gpu_radeon">gpu_radeon</option>
-                        <option <?php if ($typeComponent == "fan"){echo 'selected';}?>value="fan">fan</option>
-                        <option <?php if ($typeComponent == "opslag"){echo 'selected';}?>value="opslag">opslag</option>
-                        <option <?php if ($typeComponent == "psu"){echo 'selected';}?>value="psu">psu</option>
-                        <option <?php if ($typeComponent == "kabels"){echo 'selected';}?>value="kabels">kabels</option>
+                        <option <?php if ($typeComponent == "case_atx") {echo 'selected';} ?> value="case_atx">case_atx</option>
+                        <option <?php if ($typeComponent == "case_microatx") {echo 'selected';} ?> value="case_microatx">case_microatx</option>
+                        <option <?php if ($typeComponent == "case_miniitx") {echo 'selected';} ?> value="case_miniitx">case_miniitx</option>
+                        <option <?php if ($typeComponent == "cpu_amd") {echo 'selected';} ?> value="cpu_amd">cpu_amd</option>
+                        <option <?php if ($typeComponent == "cpu_intel") {echo 'selected';} ?> value="cpu_intel">cpu_intel</option>
+                        <option <?php if ($typeComponent == "ram") {echo 'selected';} ?> value="ram">ram</option>
+                        <option <?php if ($typeComponent == "cpukoeler") {echo 'selected';} ?> value="cpukoeler">cpukoeler</option>
+                        <option <?php if ($typeComponent == "mobo_atx") {echo 'selected';} ?> value="mobo_atx">mobo_atx</option>
+                        <option <?php if ($typeComponent == "mobo_microatx") {echo 'selected';} ?> value="mobo_microatx">mobo_microatx</option>
+                        <option <?php if ($typeComponent == "mobo_miniitx") {echo 'selected';} ?> value="mobo_miniitx">mobo_miniitx</option>
+                        <option <?php if ($typeComponent == "gpu_nvidia") {echo 'selected';} ?> value="gpu_nvidia">gpu_nvidia</option>
+                        <option <?php if ($typeComponent == "gpu_radeon") {echo 'selected';} ?> value="gpu_radeon">gpu_radeon</option>
+                        <option <?php if ($typeComponent == "fan") {echo 'selected';} ?> value="fan">fan</option>
+                        <option <?php if ($typeComponent == "opslag") {echo 'selected';} ?> value="opslag">opslag</option>
+                        <option <?php if ($typeComponent == "psu") {echo 'selected';} ?> value="psu">psu</option>
+                        <option <?php if ($typeComponent == "kabels") {echo 'selected';} ?> value="kabels">kabels</option>
                     </select>
                     <label>merk</label>
-                    <input type="text" name="merk" value="<?php echo $merk?>">
+                    <input type="text" name="merk" value="<?php echo $merk ?>">
                     <label>prijs</label>
-                    <input type="number" name="prijs" value="<?php echo $prijs?>">
+                    <input type="number" name="prijs" value="<?php echo $prijs ?>">
                     <div class="input-group">
-                        <?php if ($update == true){ ?>
-                            <button class="btn" type="submit" name="update" style="background: #556B2F;" >update</button>
-                            <button class="btn" type="submit" name="cancel" style="background: orangered;" >cancel</button>
-                        <?php }else{ ?>
-                            <button class="btn" type="submit" name="save" >Save</button>
+                        <?php if ($update == true) { ?>
+                            <button class="btn" type="submit" name="update" style="background: #556B2F;">update</button>
+                            <button class="btn" type="submit" name="cancel" style="background: orangered;">cancel
+                            </button>
+                        <?php } else { ?>
+                            <button class="btn" type="submit" name="save">Save</button>
                         <?php } ?>
                     </div>
                 </form>
